1f7cee40d8f77a5cd29f0a9449c354695bec6bd1
Taisuke Fukuno <fukuno@jig.jp>
2021-01-22 08:01:15 +0900

    update

--- download_images.js
+++ download_images.js
@@ -2,7 +2,7 @@ import { CSV } from "https://code4sabae.github.io/js/CSV.js";
 
 const data = CSV.toJSON(CSV.decode(await Deno.readTextFile("yamanashinoshoku2.csv")));
 
-const host = "https://code4sabae.github.io/yamanashishoku/";
+const host = "https://code4sabae.github.io/yamanashinoshoku/";
 await Deno.mkdir("image", { recursive: true });
 for (const d of data) {
     const bin = new Uint8Array(await (await fetch(d["写真URL"])).arrayBuffer());
